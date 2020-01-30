95656d1b7631981b16f5b22351d702d2ffcf5ef8
Giovani de Biagi Alves <giovani.alves@upx.com>
2020-01-30 19:40:00 -0300

    ajustes de acesso ao banco

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
+require('dotenv').config();
 
-mongoose.connect('mongodb://Giovani:Le201201..@200.219.149.206:27017/noderest', { useNewUrlParser : true });
+mongoose.connect(`mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASSWORD}@cluster0-rmgsv.mongodb.net/test?retryWrites=true&w=majority`, {
+    useNewUrlParser : true,
+    useUnifiedTopology: true
+});
+
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 
